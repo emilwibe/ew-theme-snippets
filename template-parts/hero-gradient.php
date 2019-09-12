5a8aaecea74d5873b36814ffb8c53dd821b1a1f8
Emil Wibe <mail@emilwibe.dk>
2019-09-12 20:37:42 +0200

    s

--- template-parts/hero-gradient.php
+++ template-parts/hero-gradient.php
@@ -9,7 +9,7 @@
   shuffle($hero_gradients);
 ?>
 <style>
-  .particles-js-canvas-el, .edge-top, .main a:hover{
+  .particles-js-canvas-el, .edge-top, .main a{
     background-image:linear-gradient(to right, <?php echo $hero_gradients[0][0]; ?> , <?php echo $hero_gradients[0][1]; ?> <?php if($hero_gradients[0][2]) : echo ", " . $hero_gradients[0][2]; endif; ?>);
   }
 </style>
