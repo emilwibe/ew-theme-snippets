2232763bcf05e3e31c60e6a5f0a6f4545e3be9ca
Emil Wibe <mail@emilwibe.dk>
2019-09-13 06:46:15 +0200

    s

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-  <section class="l-wrapper s">
+  <section class="l-wrapper l row lg-col-75-25 md-col-75-25 sm-col-100">
     <?php get_template_part('template-parts/loop'); ?>
   </section><!--/.l-wrapper.s-->
 
