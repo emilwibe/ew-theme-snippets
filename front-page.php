0755a0e7a123b35dd9d212284ac39ec51b252bad
Emil Wibe <mail@ew.dk>
2019-09-26 10:53:13 +0200

    s

--- front-page.php
+++ front-page.php
@@ -2,6 +2,8 @@
 
   <section class="l-wrapper m row lg-col-66-33 md-col-75-25 sm-col-100">
     <?php get_template_part('template-parts/loop'); ?>
+
+    <?php get_template_part('template-parts/widgetarea', 'prim_widgets'); ?>
   </section><!--/.content-grid-->
 
 <?php get_footer(); ?>
