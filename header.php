f5307d486d191c7cfa334c1f3a12f1c782fb2b53
Emil Wibe <mail@ew.dk>
2019-09-26 13:03:19 +0200

    s

--- header.php
+++ header.php
@@ -5,7 +5,7 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-  <?php get_template_part('template-parts/hero', 'gradient'); ?>
+  <?php //get_template_part('template-parts/hero', 'gradient'); ?>
   
   <?php wp_head(); ?>
 </head>
