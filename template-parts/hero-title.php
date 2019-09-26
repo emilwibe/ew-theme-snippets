49df89e137ce2d42a382f9865e8db91673718982
Emil Wibe <mail@ew.dk>
2019-09-26 13:23:11 +0200

    s

--- template-parts/hero-title.php
+++ template-parts/hero-title.php
@@ -10,7 +10,7 @@
 
     <?php elseif($_GET['s']) : ?>
 
-        <h1 class="title"><?php echo __('Search Result:', 'snippets'); ?> <?php echo $_GET['s']; ?></h1>
+        <h1 class="title"><?php echo __('Search Result:', 'snippetsdk'); ?>: <?php echo $_GET['s']; ?></h1>
 
     <?php else : ?>
 
