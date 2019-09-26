fe05b12cbd05db2db94e150d73453eac095fdcf9
Emil Wibe <mail@ew.dk>
2019-09-26 14:00:47 +0200

    s

--- template-parts/analytics.php
+++ template-parts/analytics.php
@@ -1,4 +1,4 @@
-<?php if(is_user_logged_in()) : ?>
+<?php if(!is_user_logged_in()) : ?>
 
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-148810719-1"></script>
