c8f9f1fbcc7a09af0f00ed314af98a8490d16c39
Emil Wibe <mail@ew.dk>
2019-09-26 13:05:34 +0200

    s

--- front-page.php
+++ front-page.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-  <section class="l-wrapper m row lg-col-66-33 md-col-75-25 sm-col-100">
+  <section class="l-wrapper l row lg-col-66-33 md-col-75-25 sm-col-100">
     <?php get_template_part('template-parts/loop'); ?>
 
     <?php get_template_part('template-parts/widgetarea', 'prim_widgets'); ?>
