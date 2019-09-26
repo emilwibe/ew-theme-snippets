5d56f1a76dd44f3699a3d37bcd9dd81d0a0efe5c
Emil Wibe <mail@ew.dk>
2019-09-26 10:50:49 +0200

    s

---
+++ template-parts/widgetarea-prim_widgets.php
@@ -0,0 +1,11 @@
+<?php if(is_active_sidebar('prim_widgets')) : ?>
+
+<aside>
+
+  <div id="widget-area-primary" class="widget-area primary">
+    <?php dynamic_sidebar('prim_widgets'); ?>
+  </div>
+
+</aside>
+
+<?php endif; ?>
