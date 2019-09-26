2d28299db40499ea0667332bc7ca3125f7ff7907
Emil Wibe <mail@ew.dk>
2019-09-26 13:57:05 +0200

    s

--- template-parts/analytics.php
+++ template-parts/analytics.php
@@ -1,3 +1,5 @@
+<?php if(is_user_logged_in()) : ?>
+
 <!-- Global site tag (gtag.js) - Google Analytics -->
 <script async src="https://www.googletagmanager.com/gtag/js?id=UA-148810719-1"></script>
 <script>
@@ -7,3 +9,4 @@
 
     gtag('config', 'UA-148810719-1', , { 'anonymize_ip': true });
 </script>
+<?php endif; ?>
