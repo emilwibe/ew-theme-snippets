adf448faa517903a555fad704c7c6f702027460c
Emil Wibe <mail@ew.dk>
2019-09-26 10:42:21 +0200

    s

--- functions.php
+++ functions.php
@@ -14,3 +14,15 @@
       'nav_primary' => 'Primary'
     ));
   });
+
+  //REGISTER WIDGETS
+    add_action('widgets_init', function(){
+        register_sidebar(array(
+        'name' => 'primary_widgets',
+        'id' => 'prim_widgets',
+        'before_widget' => '<div>',
+        'after_widget' => '</div>',
+        'before_title' => '<h2 class="widget_heading">',
+        'after_title' => '</h2>'
+        ));
+    });
