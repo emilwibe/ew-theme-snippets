ee6580c13f1b79187ba7d528aa40e95b59627f1a
Emil Wibe Lolk <emilwibe@ASPITPCNV07-1.local>
2019-10-22 11:36:22 +0200

    s

--- template-parts/loop.php
+++ template-parts/loop.php
@@ -6,7 +6,7 @@
         <article>
     <?php if(is_home() || is_category() || is_tag()) : ?>
 
-            <h2><a href="<?php echo get_the_permalink(); ?>"><?php the_title(); ?></a></h2>
+            <h2><a href="<?php echo get_the_permalink(); ?>" class="link article"><?php the_title(); ?></a></h2>
 
             <div class="tags"><?php the_tags(); ?></div>
 
