5e5c741248a7f759e4cf7dc8a8559307494765e2
Emil Wibe Lolk <emilwibe@ASPITPCNV07-1.local>
2019-10-25 11:04:46 +0200

    s

--- functions.php
+++ functions.php
@@ -1,12 +1,11 @@
 <?php
-  add_theme_support('title-tag');
+  //THEME SUPPORT
+  require_once('functions/theme-support');
 
   //SCRIPTS AND STYLES
-  add_action('wp_enqueue_scripts', function(){
-    wp_enqueue_style('core-styles', get_template_directory_uri() . '/dist/css/core.css');
-    wp_enqueue_script('scripts-footer', get_template_directory_uri() . '/dist/js/scripts-footer.js', '', '', true);
-    wp_enqueue_script('particles-js', get_template_directory_uri() . '/dist/js/particles.js', '', array('scripts-footer'), true);
-  });
+  require_once ('functions/scripts-styles.php');
+
+
 
   //the_excerpt
   add_filter('excerpt_length', function($length) {
