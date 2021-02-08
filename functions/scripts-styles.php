2465db723a918d6d5da03bdfb925a0cac542b066
Emil Wibe <mail@ew.dk>
2021-02-08 11:20:14 +0100

    [fix] paths to highlightjs lib

--- functions/scripts-styles.php
+++ functions/scripts-styles.php
@@ -4,7 +4,7 @@
     wp_enqueue_style('core-styles', get_template_directory_uri() . '/dist/css/core.min.css');
     wp_enqueue_script('scripts-footer', get_template_directory_uri() . '/dist/js/scripts-footer.js', '', '', true);
     wp_enqueue_script('particles-js', get_template_directory_uri() . '/dist/js/particles.js', '', array('scripts-footer'), true);
-    wp_enqueue_script('highlightJS', get_template_directory_uri() . '/dist/js/highlight.js', '', '', true);
+    wp_enqueue_script('highlightJS', get_template_directory_uri() . '/dist/js/highlightjs.js', '', '', true);
   });
 
   //DISABLE DEFAULT GUTENBERG STYLES
