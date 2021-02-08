41802634c1130f6aea523af9cfbc6960ec549906
Emil Wibe <mail@ew.dk>
2021-02-08 11:00:36 +0100

    [fix] paths to highlightjs lib

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
   <?php wp_footer(); ?>
-  
-  <script>hljs.initHighlightingOnLoad();</script>
+
+  <script>//hljs.initHighlightingOnLoad();</script>
   <?php get_template_part('template-parts/index', 'particles'); ?>
 </body>
 </html>
