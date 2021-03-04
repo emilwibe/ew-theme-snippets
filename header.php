a36b44eb7bd70c10cbf62c34759a56aea5ada56e
Emil Wibe <mail@ew.dk>
2021-03-04 21:51:21 +0100

    [refactor] Analytics script inserted by plugin to acc. GDPR

--- header.php
+++ header.php
@@ -1,8 +1,7 @@
 <!DOCTYPE html>
 <html <?php language_attributes(); ?>>
 <head>
-  <?php get_template_part('template-parts/analytics'); ?>
-
+  
   <meta <?php bloginfo('charset'); ?>>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <meta http-equiv="X-UA-Compatible" content="ie=edge">
